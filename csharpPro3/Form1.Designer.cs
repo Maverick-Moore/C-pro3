ce4707c1d5699544d89c426cbaf8866700a1216c
Maverick <111909031+Maverick-Moore@users.noreply.github.com>
2023-06-03 10:32:36 -0400

    Add files via upload

---
+++ csharpPro3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Image_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.txtImageInfo = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(693, 54);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoad.TabIndex = 0;
+            this.BtnLoad.Text = "Load Image";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(99, 76);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(406, 216);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // txtImageInfo
+            // 
+            this.txtImageInfo.Location = new System.Drawing.Point(594, 132);
+            this.txtImageInfo.Multiline = true;
+            this.txtImageInfo.Name = "txtImageInfo";
+            this.txtImageInfo.Size = new System.Drawing.Size(253, 160);
+            this.txtImageInfo.TabIndex = 0;
+            this.txtImageInfo.TextChanged += new System.EventHandler(this.txtImageInfo_TextChanged);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.Title = "Load Image";
+            this.ofd.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.txtImageInfo);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.BtnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.TextBox txtImageInfo;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ csharpPro3/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Image_Info
+{
+    public partial class Form1 : Form
+    {
+        enum PropertyID
+        {
+
+            PropertyTagDateTime = 0x0132,
+            PropertyTagExifDTOrig = 0x9003,
+            PropertyTagEquipMake = 0x010F,
+            PropertyTagEquipModel = 0x0110
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            txtImageInfo.Clear();
+
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ofd.Filter = "JPEGS|*.jpg|GIFs|*.gif|PNGs|*.png|TIF|*.tif";
+            
+            if (ofd.ShowDialog() != DialogResult.Cancel)
+            {
+                
+                pbImage.Load(ofd.FileName);
+                Image img = Image.FromFile(ofd.FileName);
+                string imageType = "";
+
+                if (ImageFormat.Jpeg.Equals(img.RawFormat))
+                {
+
+                    imageType = "JPEG Image";
+                    GetPropItems(img, "Date and Time Taken: ", (int)PropertyID.PropertyTagDateTime);
+                    GetPropItems(img, "\r\nCamera Maker: ", (int)PropertyID.PropertyTagEquipMake);
+                    GetPropItems(img, "\r\nCamera Model: ", (int)PropertyID.PropertyTagEquipModel);
+
+                }
+                else if (ImageFormat.Gif.Equals(img.RawFormat))
+                {
+
+                    imageType = "GIF Image";
+
+                }
+                else if (ImageFormat.Tiff.Equals(img.RawFormat))
+                {
+
+                    imageType = "TIFF Image";
+
+                }
+                else if (ImageFormat.Png.Equals(img.RawFormat))
+                {
+
+                    imageType = "PNG Image";
+
+                }
+
+                string imageWidth = img.Width.ToString();
+                string imageHeight = img.Height.ToString();
+                string imageResolution = img.HorizontalResolution.ToString() + " dpi";
+                string imagePixelDepth = Image.GetPixelFormatSize(img.PixelFormat).ToString();
+
+
+                txtImageInfo.Text += imageType + "\r\n";
+                txtImageInfo.Text += "Width = " + imageWidth + "\r\n";
+                txtImageInfo.Text += "Height = " + imageHeight + "\r\n";
+                txtImageInfo.Text += "Resolution = " + imageResolution + "\r\n";
+                txtImageInfo.Text += "Pixel Depth = " + imagePixelDepth + "\r\n";
+
+                int colorPaletteLength = img.Palette.Entries.Length;
+                string[] colorsArray = new string[colorPaletteLength];
+                
+                if (img.Palette != null && (colorPaletteLength > 0 && colorPaletteLength < 257))
+                {
+                    for (int i = 0; i < colorPaletteLength; i++)
+                    {
+                        string htmlColor = ColorTranslator.ToHtml(img.Palette.Entries[i]);
+                        colorsArray[i] = htmlColor;
+                    }
+                    
+                    List<string> removeDuplicateColors = new List<string>();
+
+                    foreach (var item in colorsArray)
+                    {
+                        if (removeDuplicateColors.Contains(item) == false)
+                        {
+                            removeDuplicateColors.Add(item);
+                        }
+                    }
+
+                    int hexCount = 0;
+
+                    foreach (var hexVal in removeDuplicateColors)
+                    {
+                        ColorLabel("color" + hexCount.ToString(), hexVal);
+                        hexCount++;
+                    }
+                }
+            }
+
+           
+        }
+
+        private void ColorLabel(string labelName, string labelColor)
+        {
+            var findLabel = Controls.Find(labelName, false).FirstOrDefault();
+            
+            if (findLabel != null)
+            {
+                findLabel.BackColor = ColorTranslator.FromHtml(labelColor);
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void txtImageInfo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetPropItems(Image img, string message, int ID)
+        {
+            try
+            {
+                PropertyItem propItem = img.GetPropertyItem(ID);
+                if (propItem != null)
+                {
+                    ASCIIEncoding encod = new ASCIIEncoding();
+                    string asciiInfo = encod.GetString(propItem.Value, 0, propItem.Len);
+                    txtImageInfo.Text += message + asciiInfo;
+                }
+            }
+            catch (Exception)
+            {
+                txtImageInfo.Text += message + "Not Available";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int offsetX = 0;
+            int offsetY = 0;
+            int labelWidth = 30;
+            int labelHeight = 30;
+            
+            for (int i = 0; i < 256; i++)
+            {
+                if (offsetX > 700)
+                {
+                    offsetX = 0;
+                    offsetY += labelHeight;
+                }
+
+                Controls.Add(new Label
+                {
+                    Name = "color" + (i).ToString(),
+                    Size = new Size(labelWidth, labelHeight),
+                    Location = new Point(labelWidth + offsetX, 400 + offsetY),
+                    BackColor = SystemColors.ControlDark,
+                    Text = (i).ToString()
+                });
+                offsetX += 30;
+            }
+        }
+    }
+
+}
